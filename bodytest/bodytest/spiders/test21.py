69ff3e20d2bb6de7cb535d5528c6290ca5d58b61
Maxwell Foxman <m.foxman@gmail.com>
2013-05-10 19:15:44 -0400

    Final Semester Edits
    Trying to get the code working close to successfully but not quite.

---
+++ bodytest/bodytest/spiders/test21.py
@@ -0,0 +1,61 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from scrapy.http import Request
+from bodytest.items import BodytestItem
+
+#This was a test of both the multiple pages on the thread and the multiple threads on a given page section. 
+#It does not include the multiple pages of the section.
+#This might have been one of the more successful scrapes, but I believe wouldn't scrape the first page, hence
+#why I went to parse_start_url instead
+
+class MySpider(CrawlSpider):
+    name = "xpath"
+    allowed_domains = ["genofond.org"]
+    start_urls = ["http://genofond.org/viewforum.php?f=9"]
+
+    rules = (
+            # extract and follow the forum's page links
+            Rule(SgmlLinkExtractor(restrict_xpaths="//*[@id='pagecontent']/table[2]/tr/td[2]")),
+            Rule(SgmlLinkExtractor(allow='.&t=\d+'), callback='parse_topic'),
+            Rule(SgmlLinkExtractor(allow='.&t=\d+&start=\d+'), callback='parse_topic', follow=True),
+            Rule(SgmlLinkExtractor(deny='.&p=\S+')),
+            Rule(SgmlLinkExtractor(deny='.&t=\S+&view=next')),
+            Rule(SgmlLinkExtractor(deny='.&t=\S+&view=previous')),
+            Rule(SgmlLinkExtractor(deny='.&mode=reply&t=\d+'))
+            )
+
+    #changed to parse_start_url to scrape the first page and onwards... UNSURE if it is necessary to do for final code
+    def parse_topic(self, response):
+        x = HtmlXPathSelector(response)
+        # get the list of posters
+        posters = x.select("//b[@class='postauthor']/text()").extract()
+        # set the first in list of posters as topic author
+        op = posters[0]
+        # get the topic url and title
+        url = response.url
+
+        title = x.select("//div[@id='pageheader']/h2/a/text()").extract()
+
+        #scrape topic body 
+        #But this scrape is not quite working. It is posting 
+        #the entire body of the page and not just the specific loop.
+        #I am not sure why at all
+        post_body = x.select("//div[@class='postbody']").extract()
+
+        # go through list of posters and remove any duplicates
+        posters_export = [op]
+        for p in posters:
+                posters_export.append(p)
+        # create an item for each unique poster in the topic
+        topics = []
+        for i, pb in enumerate(post_body):
+            topic = BodytestItem()
+            topic['topic_url'] = url
+            topic['topic_title'] = title
+            topic['thread_author'] = op
+            topic['post_author'] = posters[i]
+            topic['post_body'] = pb
+            topics.append(topic)
+
+        return topics
